962d0e682878d1ba7aa8807ff232eadff569a8d3
jovan <jvaonam@me.com>
2023-11-11 14:26:42 -0600

    feat: chapters one to five

---
+++ app/ui/fonts.ts
@@ -0,0 +1,20 @@
+import { Inter, Lusitana, Fira_Code, Press_Start_2P } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const fira = Fira_Code({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const pressStart2p = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+});
